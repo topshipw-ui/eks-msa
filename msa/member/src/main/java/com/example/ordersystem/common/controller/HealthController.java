fe854dad9d5c972e4e31db48c5fb22f078aeec58
topshipw <topshipw@gmail.com>
2026-03-03 17:24:48 +0900

    msa service ok10

--- msa/member/src/main/java/com/example/ordersystem/common/controller/HealthController.java
+++ msa/member/src/main/java/com/example/ordersystem/common/controller/HealthController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
     @GetMapping("/health")
     public String healthCheck(){
-        return "ok";
+        return "ok10";
     }
 }
